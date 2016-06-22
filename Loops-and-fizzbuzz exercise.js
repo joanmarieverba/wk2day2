4170a31ca1fa2f1aeedcd515da5331c241b8f35e
Joan Marie Verba <verba001@umn.edu>
2016-06-22 09:43:09 -0500

    corrected file

--- Loops-and-fizzbuzz exercise.js
+++ Loops-and-fizzbuzz exercise.js
@@ -52,16 +52,16 @@ console.assert(sum(4, 100) === 104);
  */
 
  function GCD(a, b) {
-   a = first;
-   b = last;
-   remainder = 100;
-   while(remainder !== 0){
-     remainder = first % last;
-     if (remainder !== 0 ) {first = last; last = remainder;}
-     console.log(remainder);
-   }
-  return last;
- }
+  var first = a;
+  var last = b;
+  var remainder = 100;
+  while(remainder !== 0){
+    remainder = first % last;
+    if (remainder !== 0 ) {first = last; last = remainder;}
+    console.log(remainder);
+  }
+ return last;
+};
 
 console.assert(GCD(5,1) === 1);
 console.assert(GCD(15,3) === 3);
@@ -74,14 +74,13 @@ console.assert(GCD(50,20) === 10);
  * write a function that prints out the Least Common Multiple of two numbers
  */
 
-function LCM(a, b){
-  return ((a * b) / GCD(a,b))
-}
+ function LCM(a, b){
+   if (a === 0) {return 1;}
+   else
+   {console.log ((a * b) / GCD(a,b));
+   return ((a * b) / GCD(a,b));}
+ }
 
-console.assert(LCM(10,10) === 10)
-console.assert(LCM(2,5) === 10)
-console.assert(LCM(3,6) === 6)
-console.assert(LCM(0,1) === 1)
 
 /**
  * Part 4
@@ -92,27 +91,32 @@ console.assert(LCM(0,1) === 1)
  * - for every number that is a multiple of 5 (but not 3), return "buzz"
  * - for every number that is a multiple of 3 and 5, return "fizzbuzz"
  */
-
  function fizzbuzz(N){
-   fizzBuzzString = "";
+   var fizzBuzzString = "";
    for (var i = 1; i <= N; i++) {
-   if ((N % 15) === 0) {
+
+     if ((i % 3 !== 0) && (i % 5 !== 0)) {
+       fizzBuzzString = fizzBuzzString + ".";
+   }
+
+   else if ((i % 15) === 0) {
+
        fizzBuzzString = fizzBuzzString + "fizzbuzz";
-   } else if ((N % 3) === 0) {
+   } else if ((i % 3) === 0) {
        fizzBuzzString = fizzBuzzString + "fizz";
-   } else if ((N % 5) === 0) {
+   } else if ((i % 5) === 0) {
        fizzBuzzString = fizzBuzzString + "buzz";
    }
 
-   if ((N % 3 !== 0) && (N % 5 !== 0)) {
-       fizzBuzzString = fizzBuzzString + ".";
+
    }
- }
+   console.log (fizzBuzzString);
    return fizzBuzzString;
+
  }
 
-console.assert(fizzbuzz(1) === ".")
-console.assert(fizzbuzz(2) === "..")
-console.assert(fizzbuzz(3) === "..fizz")
-console.assert(fizzbuzz(5) === "..fizz.buzz")
-console.assert(fizzbuzz(10) === "..fizz.buzzfizz..fizzbuzz")
+console.assert(fizzbuzz(1) === ".");
+console.assert(fizzbuzz(2) === "..");
+console.assert(fizzbuzz(3) === "..fizz");
+console.assert(fizzbuzz(5) === "..fizz.buzz");
+console.assert(fizzbuzz(10) === "..fizz.buzzfizz..fizzbuzz");
